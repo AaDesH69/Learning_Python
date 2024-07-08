63129ee64017b9529e8631c1eec0322661c89c47
Aadesh Pokharel <aadeshpkrel@gmail.com>
2024-07-08 15:41:26 +0545

    new update

---
+++ numpy_class/main.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+""" #Creating 1D Numpy Array
+arr1 = np.array([1, 2, 3])
+print(arr1) """
+
+""" #Creating 2D Numpy Array
+arr2 = np.array(([1, 2, 3],[4, 5, 6]))
+print(arr2) """
+
+""" #Creating 3D numpy Array
+arr3 = np.array((([1, 2, 3]), ([4, 5, 6]), ([7, 8, 9])))
+print(arr3) """
+
+""" #Creating 3D numpy Array
+arr4 = np.array([
+    [[1, 2, 3], [4, 5, 6]],
+    [[7, 8, 9], [10, 11, 12]],
+    [[13, 14, 15], [16, 17, 18]]
+])
+print(arr4)
+
+arr5 = np.array([
+    [[4, 22, 3], [32, 523, 534]],
+    [[72, 84, 94], [120, 1321, 12342]],
+    [[132, 142, 153], [146, 1473, 138]]
+])
+print(arr4)
+
+#Sum of 2 3D Array
+array_Sum = arr4 + arr5
+print(array_Sum) """
+
+""" #To Create Zero
+zero = np.zeros((3,3))
+print(zero) """
+
+""" #Creating Random Matrix
+random = np.random.random((2, 3))
+print(random) """
+
+""" #Array Operator
+#Array Operators is used to calculate between arrays
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+sum = arr1 + arr2
+print(sum)
+diff = arr1 - arr2
+print(diff)
+ """
+
+""" #slicing of Array
+slice = np.array([1, 2, 3, 4, 5])
+print(slice[:3]) """
+
+""" #Reshaping Array
+re_array = np.array([1, 2, 3, 4, 5, 6])
+re_shapped_Array = re_array.reshape((2, 3))
+print(re_shapped_Array) """
+
