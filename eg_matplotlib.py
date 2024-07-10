bf54beab760e2a8e679ce7a2a77cb73d08e0c203
Aadesh Pokharel <aadeshpkrel@gmail.com>
2024-07-10 13:49:21 +0545

    New Update

---
+++ eg_matplotlib.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+
+""" fig, ax = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 5, 30, 55]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.show() """
+
+""" #To load dataset from ploty we should istall it
+#pip install plotly
+#pip install plotly-express
+
+import plotly.express as ps
+df = ps.data.iris()
+fig = ps.scatter(df, x="sepal_width", y="sepal_length", color="species")
+fig.show() """
+
+""" #To show scatter plot and to add and xlable , ylable and title
+import plotly.express as ps
+plt.scatter([1, 2, 3, 4], [10, 20, 25, 30])
+plt.xlabel('X-axis Lable')
+plt.ylabel('y-axis Lable')
+plt.title('Scatterplot Example')
+plt.show() """
+
+""" #To sketch line plot
+import plotly.express as ps
+plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
+plt.xlabel('X-axis Lable')
+plt.ylabel('y-axis Lable')
+plt.title('Scatterplot Example')
+plt.show() """
+
+#To create bar graph
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(19680801)
+
+
+def gradient_image(ax, direction=0.3, cmap_range=(0, 1), **kwargs):
+    """
+    Draw a gradient image based on a colormap.
+
+    Parameters
+    ----------
+    ax : Axes
+        The Axes to draw on.
+    direction : float
+        The direction of the gradient. This is a number in
+        range 0 (=vertical) to 1 (=horizontal).
+    cmap_range : float, float
+        The fraction (cmin, cmax) of the colormap that should be
+        used for the gradient, where the complete colormap is (0, 1).
+    **kwargs
+        Other parameters are passed on to `.Axes.imshow()`.
+        In particular, *cmap*, *extent*, and *transform* may be useful.
+    """
+    phi = direction * np.pi / 2
+    v = np.array([np.cos(phi), np.sin(phi)])
+    X = np.array([[v @ [1, 0], v @ [1, 1]],
+                  [v @ [0, 0], v @ [0, 1]]])
+    a, b = cmap_range
+    X = a + (b - a) / X.max() * X
+    im = ax.imshow(X, interpolation='bicubic', clim=(0, 1),
+                   aspect='auto', **kwargs)
+    return im
+
+
+def gradient_bar(ax, x, y, width=0.5, bottom=0):
+    for left, top in zip(x, y):
+        right = left + width
+        gradient_image(ax, extent=(left, right, bottom, top),
+                       cmap=plt.cm.Blues_r, cmap_range=(0, 0.8))
+
+
+fig, ax = plt.subplots()
+ax.set(xlim=(0, 10), ylim=(0, 1))
+
+# background image
+gradient_image(ax, direction=1, extent=(0, 1, 0, 1), transform=ax.transAxes,
+               cmap=plt.cm.RdYlGn, cmap_range=(0.2, 0.8), alpha=0.5)
+
+N = 10
+x = np.arange(N) + 0.15
+y = np.random.rand(N)
+gradient_bar(ax, x, y, width=0.7)
+plt.show()
